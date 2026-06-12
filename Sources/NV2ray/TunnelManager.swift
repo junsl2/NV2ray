e7140bfc21a7ef172e30d233bf9b1702fb61136e
junsl2 <167585999+junsl2@users.noreply.github.com>
2026-06-13 00:35:07 +0300

    Fix Swift continuation result types

--- Sources/NV2ray/TunnelManager.swift
+++ Sources/NV2ray/TunnelManager.swift
@@ -106,7 +106,7 @@ final class TunnelManager {
     }
 
     private func save(_ manager: NETunnelProviderManager) async throws {
-        try await withCheckedThrowingContinuation { continuation in
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             manager.saveToPreferences { error in
                 if let error { continuation.resume(throwing: error) }
                 else { continuation.resume(returning: ()) }
@@ -115,7 +115,7 @@ final class TunnelManager {
     }
 
     private func load(_ manager: NETunnelProviderManager) async throws {
-        try await withCheckedThrowingContinuation { continuation in
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             manager.loadFromPreferences { error in
                 if let error { continuation.resume(throwing: error) }
                 else { continuation.resume(returning: ()) }
